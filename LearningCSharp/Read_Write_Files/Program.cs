ec9ebca86352fc7677901ef4cd13f01d5d20b9de
Ziata07 <mark.james0712@gmail.com>
2021-08-16 07:46:48 -0400

    Read_Write_Updates
    Learning logic for Streamreader.Read methods

--- LearningCSharp/Read_Write_Files/Program.cs
+++ LearningCSharp/Read_Write_Files/Program.cs
@@ -5,14 +5,14 @@ namespace Read_Write_Files
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main_0(string[] args)
         {
             String line;
+            int x;
             try
             {
                 //Pass the file path and file name to the StreamReader/StreamWriter constructor
-                StreamReader sr = new StreamReader("F:\\CSharpProject\\LearningCSharp\\Read_Write_Files\\NotepadFolder\\ReadFromThis.txt");
-                StreamWriter sw = new StreamWriter("F:\\CSharpProject\\LearningCSharp\\Read_Write_Files\\NotepadFolder\\NewWrittenFile.txt");
+                StreamReader sr = new StreamReader("F:\\LearningProgramming\\LearningCSharp\\Read_Write_Files\\NotepadFolder\\ReadFromThis.txt");
                 //Read the first line of text
                 line = sr.ReadLine();
                 //Continue to read until you reach end of file
@@ -26,6 +26,42 @@ namespace Read_Write_Files
                 //close the file
                 sr.Close();
                 Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+
+            try
+            {   //Creating a file, writing to it, then closing the file. File path shoes WHERE file will be
+                StreamWriter sw = new StreamWriter("F:\\LearningProgramming\\LearningCSharp\\Read_Write_Files\\NotepadFolder\\NewWrittenFile.txt");
+                sw.WriteLine("This is another test! Using the WriteLine method.");
+                sw.WriteLine("This is from the StreamWriter class");
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(" Exception line: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing the finally block");
+            }
+
+            try
+            {   //There are many different constructors for StreamWriter class
+                StreamWriter anotherFile = new StreamWriter("F:\\LearningProgramming\\LearningCSharp\\Read_Write_Files\\NotepadFolder\\ThisFileWillRefresh.txt", false, System.Text.Encoding.UTF8);
+                for (x = 0; x < 10; x++)
+                {
+                    anotherFile.Write(x + "\n");
+                }
+
+                anotherFile.Close();
             }
             catch (Exception e)
             {
---
+++ LearningCSharp/Read_Write_Files/Test_ReadFile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace Read_Write_Files
+{
+    class Test_ReadFile
+    {
+        static void Main()
+        {
+           string[] ar;
+
+            ar = System.IO.File.ReadAllLines("F:\\LearningProgramming\\LearningCSharp\\Read_Write_Files\\NotepadFolder\\Even_Odd_File.txt");
+            string x;
+            int y;
+          
+            StreamReader streamReader = new StreamReader("F:\\LearningProgramming\\LearningCSharp\\Read_Write_Files\\NotepadFolder\\Even_Odd_File.txt");
+            x = streamReader.ReadLine();
+            int.TryParse(x, out y);
+
+            do
+            {
+                if (y % 2 != 0)
+                {
+                    Console.WriteLine(x);
+                }
+                x = streamReader.ReadLine();
+
+            } while (int.TryParse(x, out y));          
+        }
+    }
+}
