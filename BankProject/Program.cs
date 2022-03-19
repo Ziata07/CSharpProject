91926a1d3cc09df74f01e28ab1e274c469f817ea
Ziata07 <mark.james0712@gmail.com>
2022-03-19 14:41:00 -0400

    Committing Babk Project to git

---
+++ BankProject/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace BankProject
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Bank test = new Bank();
+			test.RunBank(test);
+		}
+	}
+}
